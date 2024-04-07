19db1f0353fc83a9280fb306d4f7adbaae9630bd
MarB-T <danmarbet@gmail.com>
2024-04-07 21:52:36 +0300

    Add session cookie

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -1,47 +1,45 @@
 #!/usr/bin/env python3
-"""
-Module for authentication
-"""
-
-from typing import List, TypeVar
+"""Aythentication method definition"""
 from flask import request
+from typing import TypeVar, List
+from os import getenv
 
 
 class Auth:
-    """a class to manage the API authentication
-    """
+    """Authentication class definition"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """public method to check if auth is required
-        """
-        if path is None:
+        """require of authentication"""
+        if path is None or not path:
             return True
-
         if excluded_paths is None or not excluded_paths:
             return True
-
-        for excluded_path in excluded_paths:
-            if excluded_path.startswith(path):
+        path = r'{}'.format(path)
+        if path[-1] == r'/':
+            path = path[:-1]
+        for pat in excluded_paths:
+            pate = r'{}'.format(pat)
+            if pate[-1] == r'/':
+                pate = pate[:-1]
+            if path == pate:
                 return False
-            elif path.startswith(excluded_path):
-                return False
-            elif excluded_path[-1] == "*":
-                if path.startswith(excluded_path[:-1]):
-                    return False
-
         return True
 
     def authorization_header(self, request=None) -> str:
-        """public method that gets auth header
-        """
+        """The authorization header"""
         if request is None:
             return None
-
-        header = request.headers.get('Authorization')
-        if header is None:
+        if not request.headers.get('Authorization'):
             return None
-        return header
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """public method to return flask object
-        """
+        """The Crrent User"""
         return None
+
+    def session_cookie(self, request=None):
+        """Session Cookie"""
+        if not request:
+            return None
+        value = getenv('SESSION_NAME')
+        return request.cookies.get(value)
+
